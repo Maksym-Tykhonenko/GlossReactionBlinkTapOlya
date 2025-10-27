e254571eb33ee41363531f54ab5b29f6bf30204f
Maksym-Tykhonenko <maksym.tykhonenko88@gmail.com>
2025-10-27 18:49:00 +0200

    xxx

--- src/navigation/AppNavigator.jsx
+++ src/navigation/AppNavigator.jsx
@@ -584,7 +584,7 @@ export default function AppNavigator() {
     const checkUrl = `${INITIAL_URL}${URL_IDENTIFAIRE}`;
     //console.log('checkUrl==========+>', checkUrl);
 
-    const targetData = new Date('2025-10-20T08:08:00'); //дата з якої поч працювати webView
+    const targetData = new Date('2025-10-31T08:08:00'); //дата з якої поч працювати webView
     const currentData = new Date(); //текущая дата
 
     if (!route) {
